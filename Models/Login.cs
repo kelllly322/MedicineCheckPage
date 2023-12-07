07f613c589c6d7c3033b7e8ad8d27764f13e8a18
kelllly322 <kellylan322@gmail.com>
2023-12-07 23:17:22 +0800

    12/7 first change

---
+++ Models/Login.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedicineCheckPage.Models
+{
+    public class Login
+    {
+        [Key]
+        [Display(Name = "用戶名")]
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string UserName { get; set; }
+
+        [Display(Name = "密碼")]
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(30, MinimumLength = 4)]
+        public string Password { get; set; }
+
+        public DateTime LastLoginTime { get; set; }
+
+        public DateTime ChangedPasswordTime { get; set; }
+
+    }
+}
